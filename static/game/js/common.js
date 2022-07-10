1891722013b8816fa833c841115de1e66f6ab528
liumingmin <443925902@qq.com>
2022-07-10 12:23:05 +0800

    feat: change to commonjs mode

--- static/game/js/common.js
+++ static/game/js/common.js
@@ -1,29 +1,7 @@
-function TextEncode(text) {
-    return new TextEncoder("utf-8").encode(text);
-}
-
-function TextDecode(uint8array) {
-    if (uint8array.length > 0) {
-        return new TextDecoder("utf-8").decode(uint8array);
-    }
-    return uint8array
-}
-
-
-function uuid() {
-    var s = [];
-    var hexDigits = "0123456789abcdef";
-    for (var i = 0; i < 36; i++) {
-        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
-    }
-    s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
-    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
-    s[8] = s[13] = s[18] = s[23] = "-";
-
-    var uuid = s.join("");
-    return uuid;
-}
-
-
 var hostAddress = "127.0.0.1:8004"
 
+const WS_C2S_DROP = 10
+const WS_C2S_RESUME = 11
+
+const WS_S2C_SCALING      = 20
+const WS_S2C_DROP_TIGER   = 21
--- static/game/js/protocol.js
+++
@@ -1,6 +0,0 @@
-const WS_C2S_DROP = 10
-const WS_C2S_RESUME = 11
-
-const WS_S2C_SCALING      = 20
-const WS_S2C_DROP_TIGER   = 21
-
